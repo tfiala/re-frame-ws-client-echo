cd97af6b0302bccbaf3355987be03b9d2a9839fa
Todd Fiala <todd.fiala@gmail.com>
2024-12-27 23:54:21 -0800

    wip: echo server and pows-based websocket-fx almost there

--- src/chat_re_frame/echo.cljs
+++ src/chat_re_frame/echo.cljs
@@ -2,33 +2,9 @@
   (:require
    [re-frame.core :as re-frame]
    [websocket-fx.pows :as wfx]
-   [chat-re-frame.subs :as subs]
-   [chat-re-frame.websocket :as ws]))
+   [chat-re-frame.subs :as subs]))
 
 (def socket-id :default)
-
-(def socket-options
-  {; optional. defaults to /ws on the current domain. ws if http and wss if https
-   :url    "wss://echo.websocket.org"
-   ; optional. defaults to :edn, options are #{:edn :json :transit-json}
-   :format :text
-   ; optional. additional event to dispatch after the socket is connected
-   :on-connect [::websocket-connected]
-   ; optional. additional event to dispatch if the socket disconnects
-   :on-disconnect [::websocket-disconnected]
-   ; tfiala - added this
-   :on-message [::ws-incoming-message]})
-
-; start the connection process (will happen sometime later)
-;; (re-frame/dispatch [::wfx/connect socket-id options])
-
-(defn start
-  []
-  (re-frame/dispatch [::wfx/connect socket-id socket-options]))
-
-(defn stop
-  []
-  (re-frame/dispatch [::wfx/disconnect socket-id]))
 
 (re-frame/reg-event-db
  ::websocket-connected
@@ -44,7 +20,7 @@
 (re-frame/reg-event-fx
  ::ws-incoming-message
  (fn [{:keys [db]} message]
-   (println "echo response received")
+   (println "echo response received: " message)
    {:db (update-in db [:messages] conj message)}))
 
 (re-frame/reg-event-db
@@ -57,35 +33,25 @@
  ::send-echo-message
  (fn [_ message]
    (println "reg-event-fx, message: " message)
-   {::wfx/ws-message {:socket-id socket-id :message message}}))
+   (re-frame/dispatch [::wfx/send-message socket-id {:message message}])))
 
 (defn websocket-status
   []
   [:h2 "websocket status"
    [:p
-    @(re-frame/subscribe [::wfx/status ws/socket-id])]])
+    @(re-frame/subscribe [::wfx/status socket-id])]])
 
 (defn echo-message
   [idx message-text]
   ^{:key idx}
   [:li message-text])
 
-(defn make-echo-request
-  [message]
-  {:message message
-   :on-response [::echo-response]
-   :on-timeout [::echo-timeout]
-   :timeout 10000})
-
-(defn dispatch-echo-request
-  [message]
-  (re-frame/dispatch [::send-echo-message (make-echo-request message)]))
-
 (defn send-button
   []
   [:button
-   {:on-click (fn [_e]
-                (dispatch-echo-request "hello, world"))}
+   {:on-click
+    (fn [_e]
+      (re-frame/dispatch [::send-echo-message socket-id "hello, world"]))}
    "Send Message"])
 
 (defn echo-view []
@@ -94,3 +60,23 @@
    [:h1 "Echo Messages"
     [:ul (map-indexed #'echo-message @(re-frame/subscribe [::subs/messages]))]
     (send-button)]])
+
+(def socket-options
+  {; optional. defaults to /ws on the current domain. ws if http and wss if https
+   :url    "wss://echo.websocket.org"
+   ; optional. defaults to :edn, options are #{:edn :json :transit-json}
+   :format :text
+   ; optional. additional event to dispatch after the socket is connected
+   :on-connect [::websocket-connected]
+   ; optional. additional event to dispatch if the socket disconnects
+   :on-disconnect [::websocket-disconnected]
+   ; tfiala - added this
+   :on-message ::ws-incoming-message})
+
+(defn start
+  []
+  (re-frame/dispatch [::wfx/connect socket-id socket-options]))
+
+(defn stop
+  []
+  (re-frame/dispatch [::wfx/disconnect socket-id]))
